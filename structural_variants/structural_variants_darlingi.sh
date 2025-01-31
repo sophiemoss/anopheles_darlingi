c20f2a56d8a072a75cdf729e6b98c4cfcc6acccf
Sophie Moss <sophie.moss04@gmail.com>
2025-01-31 10:04:04 +0000

    updates to python extraction script for genes impacted by duplications

--- structural_variants/structural_variants_darlingi.sh
+++ structural_variants/structural_variants_darlingi.sh
@@ -47,16 +47,13 @@ tabix -p vcf merged_genotyped_structural_variants_miss20.vcf.gz
 
 # Query number of variants
 bcftools view merged_genotyped_structural_variants_miss20.vcf.gz | grep -v "^#" | wc -l
-# 76472 structural variants in total across entire genome after filtered for missingness.
+# 597055 structural variants in total across entire genome after filtered for missingness.
 
 # Filter the VCF based on genes of interest
 bcftools view -R AnDar_allIR_structuralvariants1000.bed merged_genotyped_structural_variants_miss20.vcf.gz -Oz -o genes_merged_genotyped_structural_variants_miss20.vcf.gz
 tabix -p vcf genes_merged_genotyped_structural_variants_miss20.vcf.gz
 bcftools view genes_merged_genotyped_structural_variants_miss20.vcf.gz | grep -v "^#" | wc -l
-# 387 variants in genes of interest
-
-# Extract information from vcf
-bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO/SVTYPE\t[%GT\t]\n' genes_merged_genotyped_structural_variants_miss20.vcf.gz > structural_variants.txt
+# 3310 variants in genes of interest
 
 # Annotate with snpeff
 
@@ -65,79 +62,6 @@ bgzip genes_merged_genotyped_structural_variants_miss20.ann.vcf
 tabix -p vcf genes_merged_genotyped_structural_variants_miss20.ann.vcf.gz
 
 # Extract information from vcf with snpeff
-bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO/SVTYPE\t%INFO/ANN\t[%GT\t]\n' genes_merged_genotyped_structural_variants_miss20.ann.vcf.gz > structural_variants.ann.txt
+bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO/SVTYPE\t%INFO/ANN\t[%GT\t]\n' genes_merged_genotyped_structural_variants_miss20.ann.vcf.gz > structural_variants.ann.tsv
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Query what the unique Structural Variant Types are in the dataset
-# zgrep -E "SVTYPE" genes_merged_genotyped_structural_variants_sample_filt.site_filt.call_filt.FMISSING_MAF.vcf.gz | cut -f8 | grep -o "SVTYPE=[^;]*" | sort | uniq
-
-
-#####  Separate by variant type
-
-#zgrep -E '^#|SVTYPE=DUP' genes_merged_genotyped_structural_variants_sample_filt.site_filt.call_filt.vcf.gz | bgzip > duplications.vcf.gz
-#bcftools view duplications.vcf.gz | grep -v "^#" | wc -l
-#snpEff Anopheles_gambiae duplications.vcf.gz > duplications.ann.vcf
-#bgzip duplications.ann.vcf
-#tabix -p vcf duplications.ann.vcf.gz
-#
-
-### Analysis Type 1: between Bijagos and Elsewhere Fst ###
-
-# Will now look to see if there are any in the Bijagos melas (pop1) vs non-Bijagos melas (pop2)
-# Made two populations.txt files pop1.txt (Bijagos samples) and pop2.txt (cameroon/gambia samples)
-vcftools --gzvcf genes_merged_genotyped_structural_variants_sample_filt.site_filt.call_filt.vcf.gz --weir-fst-pop pop1.txt --weir-fst-pop pop2.txt --out Bijagos_vs_Elsewhere
-
-
-
-## creat mappability map
-
-dicey chop Anopheles_gambiae.AgamP4.dna.toplevel.fa
-bwa index Anopheles_gambiae.AgamP4.dna.toplevel.fa
-bwa mem Anopheles_gambiae.AgamP4.dna.toplevel.fa car1002_Combined_1.fastq.gz car1002_Combined_2.fastq.gz | samtools sort -@ 8 -o srt.bam -
-samtools index srt.bam 
-dicey mappability2 srt.bam 
-gunzip map.fa.gz && bgzip map.fa && samtools faidx map.fa.gz 
-
-
-
-### possible snpeff addition
-
-#
-#bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO/SVTYPE\t[%GT\t]\n' genes_merged_genotyped_structural_variants_sample_filt.site_filt.call_filt.ann.vcf.gz > structural_variants_snpeff.txt
-#
-
-## Looking for chromosomal inversions using compkaryo, which looks at SNPs
-
-# use the 'test' conda environment
-
-# works for '2La', '2Rj', '2Rb', '2Rc_col', '2Rc_gam', '2Rd', '2Ru'
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2L_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2La -s compkaryo_samples.txt -o compkaryo_out_2La.txt --total
-
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2R_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2Rb -s compkaryo_samples.txt -o compkaryo_out_2Rb.txt --total
-
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2R_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2Rj -s compkaryo_samples.txt -o compkaryo_out_2Rj.txt --total
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2R_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2Rc_gam -s compkaryo_samples.txt -o compkaryo_out_2Rc_gam.txt --total
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2R_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2Rd -s compkaryo_samples.txt -o compkaryo_out_2Rd.txt --total
-python /mnt/storage11/sophie/gitrepos/compkaryo/compkaryo/compkaryo.py 2R_only_2019melasglobal_finalfiltered_gambiaealigned_phased.vcf.gz 2Ru -s compkaryo_samples.txt -o compkaryo_out_2Ru.txt --total
+# Use closed excel to open the tsv when no other excels are open, so that it opens the wizard, then make sure in step 1 it says delimted, then step 2 only tsv is ticked.
